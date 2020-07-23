034a38fa26d1d45cd0c2c7be22b5f0fd5fe83440
Lenovo <Lenovo@Hari>
2020-07-23 16:16:28 +0530

    EBMain

--- Electricity_Bill/src/com/wipro/eb/main/EBMain.java
+++ Electricity_Bill/src/com/wipro/eb/main/EBMain.java
@@ -1,17 +1,13 @@
 package com.wipro.eb.main;
-
 import com.wipro.eb.service.ConnectionService;
-
 public class EBMain 
-
 {
-
 	public static void main(String a[])
-
 	{
-
-	System.out.println(new ConnectionService().generateBill(130,100,"Commercial"));
-
+		System.out.println(new ConnectionService().generateBill(90,100,"domestic"));
+		System.out.println(new ConnectionService().generateBill(150,100,"domestic"));
+		System.out.println(new ConnectionService().generateBill(190,-6,"domestic"));
+		System.out.println(new ConnectionService().generateBill(90,130,"commercial"));
+		System.out.println(new ConnectionService().generateBill(150,100,"commercial"));
 	}
-
 }
