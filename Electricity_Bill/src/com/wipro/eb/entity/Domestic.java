74d1785a7251f1b664771f7451f38a596a5a4e1c
Lenovo <Lenovo@Hari>
2020-07-23 16:12:56 +0530

    Domestic

--- Electricity_Bill/src/com/wipro/eb/entity/Domestic.java
+++ Electricity_Bill/src/com/wipro/eb/entity/Domestic.java
@@ -4,18 +4,18 @@ public class Domestic extends Connection
 	public Domestic(int currentReading, int previousReading,float slabs[])
 	{
 		super(currentReading,previousReading,slabs);
-	}
+	}	
 	public float computeBill()
 	{
 		float units=0.0f;
 		units=currentReading-previousReading;
 		double bill=0.0f;
 		if(units<51)
-			bill=units*2.3;
-		if(units>50&&units<101)
-			bill=(units-50)*4.2+115;
+			bill=units*slabs[0];
+		else if(units>50&&units<101)
+			bill=(units-50)*slabs[1]+50*slabs[0];
 		else
-			bill=(units-100)*5.5+325;
+			bill=(units-100)*slabs[2]+50*slabs[1]+50*slabs[0];
 		return (float)bill;
 	}
 }
